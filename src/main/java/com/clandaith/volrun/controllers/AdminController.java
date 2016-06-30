a9d5e70b04fcb48afd83169baf8c5132369fcc26
clandaith <clandaith@gmail.com>
2016-06-30 11:51:21 -0600

    updated stuff

--- src/main/java/com/clandaith/volrun/controllers/AdminController.java
+++ src/main/java/com/clandaith/volrun/controllers/AdminController.java
@@ -26,4 +26,76 @@ public class AdminController {
 
 		return "admin/index";
 	}
+
+	@RequestMapping("/admin/add-view-files")
+	public String addViewFiles(Model model) {
+		LOGGER.info("index");
+		// model.addAttribute("users", userService.getAllUsers());
+
+		return "admin/addViewFiles";
+	}
+
+	@RequestMapping("/admin/completed-demos")
+	public String completedDemos(Model model) {
+		LOGGER.info("completedDemos");
+		// model.addAttribute("users", userService.getAllUsers());
+
+		return "admin/completedDemos";
+	}
+
+	@RequestMapping("/admin/completed-tournaments")
+	public String completedTournaments(Model model) {
+		LOGGER.info("completedTournaments");
+		// model.addAttribute("users", userService.getAllUsers());
+
+		return "admin/completedTournaments";
+	}
+
+	@RequestMapping("/admin/current-members")
+	public String currentMembers(Model model) {
+		LOGGER.info("currentMembers");
+		// model.addAttribute("users", userService.getAllUsers());
+
+		return "admin/currentMembers";
+	}
+
+	@RequestMapping("/admin/demo-requests")
+	public String demoRequests(Model model) {
+		LOGGER.info("demoRequests");
+		// model.addAttribute("users", userService.getAllUsers());
+
+		return "admin/demoRequests";
+	}
+
+	@RequestMapping("/admin/membership-requests")
+	public String membershipRequests(Model model) {
+		LOGGER.info("membershipRequests");
+		// model.addAttribute("users", userService.getAllUsers());
+
+		return "admin/membershipRequests";
+	}
+
+	@RequestMapping("/admin/scheduled-demos")
+	public String scheduledDemos(Model model) {
+		LOGGER.info("scheduledDemos");
+		// model.addAttribute("users", userService.getAllUsers());
+
+		return "admin/scheduledDemos";
+	}
+
+	@RequestMapping("/admin/scheduled-tournaments")
+	public String scheduledTournaments(Model model) {
+		LOGGER.info("scheduledTournaments");
+		// model.addAttribute("users", userService.getAllUsers());
+
+		return "admin/scheduledTournaments";
+	}
+
+	@RequestMapping("/admin/send-message")
+	public String sendMessage(Model model) {
+		LOGGER.info("sendMessage");
+		// model.addAttribute("users", userService.getAllUsers());
+
+		return "admin/sendMessage";
+	}
 }
