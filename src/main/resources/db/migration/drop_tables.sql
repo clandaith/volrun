4edc114e6b5ddab4fdffc5b8ef195ece95a03db1
clandaith <clandaith@gmail.com>
2016-07-12 20:01:37 -0600

    asdf

---
+++ src/main/resources/db/migration/drop_tables.sql
@@ -0,0 +1,11 @@
+
+--drop sequence hibernate_sequence;
+--drop table schema_version;
+--drop table game_systems;
+--drop table game_companies;
+--drop table user_roles;
+--drop table demos;
+--drop table files;
+--drop table tournaments;
+--drop table users;
+--drop table stores;
